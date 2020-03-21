1937aa5293796199353ea4d3321160398437d590
Matthias Schedel <matthias.schedel@student.kit.edu>
2020-03-21 18:06:23 +0100

    fix models

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,9 +1,12 @@
 // Event.model.js
 const mongoose = require("mongoose");
+// const ObjectId = mongoose.Types.ObjectId();//.ObjectId();
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 const eventSchema = new mongoose.Schema({
-    _id: ObjectId,
+    id: ObjectId,
     name: String,
-    startDate: Date,    
+    startDate: Date,
     endDate: Date,
     tags: [String],
     category: [String],
@@ -11,12 +14,12 @@ const eventSchema = new mongoose.Schema({
     location: String,
     trainer: ObjectId,
     language: String,
-    participants: [{ user: ObjectId, name: String, hasPayed: Boolean}],
-    comments: [{user: ObjectId, name: String, text: String, date: Date}],
+    participants: [{ user: ObjectId, name: String, hasPayed: Boolean }],
+    comments: [{ user: ObjectId, name: String, text: String, date: Date }],
     description: String,
     sessionLink: String,
     paymentLink: String,
-    rating: [{user: ObjectId, value: Number, comment: String}],
+    rating: [{ user: ObjectId, value: Number, comment: String }],
 });
 const Event = mongoose.model("Event", eventSchema);
 module.exports = Event;
--- models/Group.model.js
+++ models/Group.model.js
@@ -1,9 +1,12 @@
 // Group.model.js
 const mongoose = require("mongoose");
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
 const groupSchema = new mongoose.Schema({
     name: String,
-    trainers: [{user: ObjectId, name: String}],
-    participants: [{user: ObjectId, name: String}],
+    trainers: [{ user: ObjectId, name: String }],
+    participants: [{ user: ObjectId, name: String }],
     description: String,
     picture: String,
 
