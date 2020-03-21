7e2aca4b2d189399806e4b2d103ab0f97d443a73
Matthias Schedel <matthias.schedel@student.kit.edu>
2020-03-21 18:07:08 +0100

    add functionality for frontend

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var bcrypt = require('bcrypt');
+var expressSession = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mainPageRouter = require('./routes/main');
+var loginPageRouter = require('./routes/login');
+var signupPageRouter = require('./routes/signup');
+var privacyPolicyPageRouter = require('./routes/privacy-policy');
+var termsOfUsePageRouter = require('./routes/terms-of-use');
+var aboutPageRouter = require('./routes/about');
+var contactPageRouter = require('./routes/contact');
 
+
+
+var User = require('./models/User.model');
+var Participant = require('./models/Participant.model');
+var Group = require('./models/Group.model');
+var Event = require('./models/Event.model');
+var Trainer = require('./models/Trainer.model');
+
+mongoose.connect('mongodb://' + process.env.MONGO_INITDB_ROOT_USERNAME + ":" + process.env.MONGO_INITDB_ROOT_PASSWORD + '@0.0.0.0:27017/' + process.env.MONGO_INITDB_DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
 var app = express();
 
 // view engine setup
@@ -18,9 +40,102 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({
+  secret: process.env.EXPRESS_SESSION_SECRET
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+  usernameField: "email",
+  passwordField: "password"
+}, function (email, password, next)
+{
+  User.findOne({
+    email: email
+  }, function (err, user)
+  {
+    if (err) return next(err);
+    if (!user || !bcrypt.compareSync(password, user.passwordHash))
+    {
+      return next({ message: 'E-Mail oder Passwort falsch' })
+    }
+    next(null, user);
+  })
+}));
+
+
+passport.use('signup-local', new LocalStrategy({
+  usernameField: "email",
+  passwordField: "password"
+}, function (email, password, next)
+{
+  console.log('signup local');
+  User.findOne({
+    email: email
+  }, function (err, user)
+  {
+    if (err) return next(err);
+    if (user) return next({ message: "Benutzer existiert bereits" });
+    let newUser = new User({
+      name: "",
+      email: email,
+      passwordHash: bcrypt.hashSync(password, 10),
+      friends: [],
+      accecptGDPR: [{ acceptedDate: new Date(), acceptedVersion: "1" }],
+      profilePic: ""
+    })
+    newUser.save(function (err)
+    {
+      next(err, newUser);
+    });
+  });
+}));
+
+passport.serializeUser(function (user, next)
+{
+  next(null, user._id);
+});
+
+passport.deserializeUser(function (id, next)
+{
+  User.findById(id, function (err, user)
+  {
+    next(err, user);
+  });
+});
+
+app.get('/logout', function (req, res, next)
+{
+  req.logout();
+  res.redirect('/');
+});
+
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login-page' }),
+  function (req, res)
+  {
+    res.redirect('/main');
+  });
+
+
+app.post('/signup',
+  passport.authenticate('signup-local', { failureRedirect: '/signup-page' }),
+  function (req, res)
+  {
+    res.redirect('/main');
+  });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login-page', loginPageRouter);
+app.use('/signup-page', signupPageRouter);
+app.use('/main', mainPageRouter);
+app.use('/privacy-policy', privacyPolicyPageRouter);
+app.use('/terms-of-use', termsOfUsePageRouter);
+app.use('/contact', contactPageRouter);
+app.use('/about', aboutPageRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next)
