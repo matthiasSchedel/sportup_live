d2387362007c28ff22fee1c06084c2669d984d9e
Kai Wiederhake <kai@wiederhake.com>
2020-03-21 17:52:50 +0100

    added group view

--- routes/group.js
+++ routes/group.js
@@ -5,8 +5,13 @@ var Group = require('../models/Group.model')
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   Group.find({}, function(error, result ) {
-    console.log("Group result", result);
     res.render('groups', { groups: result });
+  });
+});
+
+router.get('/:name', function (req, res, next) {
+  Group.findOne({ name: req.params.name}, function(error, result ) {
+    res.render('group', { group: result });
   });
 });
 
