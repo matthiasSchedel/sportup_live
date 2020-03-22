9381c5b5edff4783bb2338076fd85fbf163df587
Matthias Schedel <matthias.schedel@student.kit.edu>
2020-03-22 17:06:44 +0100

    update main page

--- routes/main.js
+++ routes/main.js
@@ -5,12 +5,16 @@ var EventService = require("../services/eventService");
 var GroupService = require("../services/groupService");
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-  try {
+router.get('/', async function (req, res, next)
+{
+  try
+  {
     var events = await EventService.get({})
     var groups = await GroupService.get({})
-    res.render('main', { events, groups, moment });
-  } catch (e) {
+    console.log(JSON.stringify(req.session.user));
+    res.render('main', { events, groups, moment, user: req.session.user || { name: 'Sportsfreund' } });
+  } catch (e)
+  {
     req.flash("error", "Fehler beim event oder gruppen laden");
     res.render("index", { events: [], moment, user: req.user });
   }
