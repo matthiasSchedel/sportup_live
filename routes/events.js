5ae1eb3c52753ed4c2db47ddccb9fc1e5194854d
Simon Bigelmayr <bigel86@gmail.com>
2020-03-22 02:09:36 +0100

    make event async await

--- routes/events.js
+++ routes/events.js
@@ -1,17 +1,66 @@
 var express = require("express");
 var router = express.Router();
-var Event = require("../models/Event.model");
+var moment = require("moment");
 var EventService = require("../services/eventService");
 
 // Retrieve all events
-router.get('/', EventService.findAll);
+router.get("/", async function(req, res, next) {
+    try {
+        var events = await EventService.getEvents({})
+        res.render("events", { events: events, moment: moment });
+    } catch (e) {
+        req.flash("error", "Fehler beim event laden");
+        res.render("events", { events: [], moment: moment });
+    }
+});
+
 // Create a new event
-router.post('/', EventService.create);
+router.post("/", async function(req, res, next) {
+    try {
+        await EventService.createEvent(req)
+        var events = await EventService.getEvents({})
+        res.render("events", { events: events, moment: moment });
+    } catch (e) {
+        req.flash("error", "Fehler beim event erstellen");
+        res.render("events", { events: [], moment: moment });
+    }
+});
+
 // Retrieve a single event with id
-router.get('/:id', EventService.findOne);
+router.get("/:id", async function(req, res, next) {
+    try {
+        var event = await EventService.findOne(req)
+        res.render("event", { event: event, moment: moment });
+    } catch (e) {
+        req.flash("error", "Fehler beim event laden");
+        res.render("event", { event: [], moment: moment });
+    }
+});
+
 // Update a event with id
-router.put('/:id', EventService.update);
+router.put("/:id", async function(req, res, next) {
+    try {
+        var event = await EventService.update(req)
+        res.render("event", { event: event, moment: moment });
+    } catch (e) {
+        req.flash("error", "Fehler beim event laden");
+        res.render("event", { event: [], moment: moment });
+    }
+});
+
 // Delete a event with id
-router.get('/delete/:id', EventService.delete);
+router.get("/delete/:id", async function(req, res, next) {
+    try {
+        var events = await EventService.delete(req)
+        req.flash("success", "Event wurde gelöscht");
+        req.method = 'GET'
+        res.redirect('/events');
+    } catch (e) {
+        console.log(e)
+        req.flash("error", "Fehler beim event löschen");
+        req.method = 'GET'
+        res.redirect('/events');
+    }
+});
 
 module.exports = router;
--- services/eventService.js
+++ services/eventService.js
@@ -1,117 +1,57 @@
 var Event = require("../models/Event.model");
-var moment = require("moment");
 const mongoose = require("mongoose");
 
-function findAll(req, res) {
-  Event.find()
-    .then(events => {
-      res.render("events", { events: events, moment: moment });
-    })
-    .catch(err => {
-      console.log(err);
-      req.flash("error", "Events konnte nicht geladen werden");
-      res.render("events", { events: [], moment: moment });
-    });
+exports.getEvents = async function (query) {
+    try {
+        return await Event.find(query)
+    } catch (e) {
+        // Log Errors
+        console.log(e);
+        throw Error('Error while Getting Events')
+    }
 }
-exports.findAll = findAll;
 
-exports.create = (req, res) => {
-  var event = assignParamsToEvent(req);
-  event._id = new mongoose.Types.ObjectId();
-
-  event
-    .save()
-    .then(response => {
-      console.log("Event Saved", response);
-      req.flash("success", "Event wurde erfolgreich gespeichert");
-      findAll(req, res);
-    })
-    .catch(err => {
-      console.log(err);
-      req.flash("error", "Events konnte nicht gespeichert werden");
-      findAll(req, res);
-    });
-};
-
-exports.findOne = (req, res) => {
-  Event.findById(req.params.id)
-    .then(event => {
-      if (!event) {
-        return eventNotFoundByIdResponse(req, res, event);
-      }
-      res.render("event", { event: event, moment: moment });
-    })
-    .catch(err => {
-      if (err.kind === "ObjectId" || err.name === "NotFound") {
-        return res.status(404).send({
-          message: "event not found with id " + req.params.id
-        });
-      }
-      return res.status(500).send({
-        message: "Could not delete event with id " + req.params.id
-      });
-    });
-};
-
-// Update a Event identified by the id in the request
-exports.update = (req, res) => {
-  // Validate Request
-  if (!req.body) {
-    console.log("Missing Parameters", req.body);
-    req.flash("error", "Fehlerhafte Parameter");
-    findAll(req, res);
-  }
-  // Find event and update it with the request body
-  Event.findByIdAndUpdate(req.params.id, assignParamsToEvent(req), {
-    new: true
-  })
-    .then(event => {
-      if (!event) {
-        return eventNotFoundByIdResponse(req, res, event);
-      }
-      res.send(event);
-    })
-    .catch(err => {
-      if (err.kind === "ObjectId" || err.name === "NotFound") {
-        return res.status(404).send({
-          message: "event not found with id " + req.params.id
-        });
-      }
-      return res.status(500).send({
-        message: "Could not delete event with id " + req.params.id
-      });
-    });
-};
-
-// Delete a Event with the specified id in the request
-exports.delete = (req, res) => {
-  Event.findByIdAndRemove(req.params.id)
-    .then(event => {
-      if (!event) {
-        return eventNotFoundByIdResponse(req, res, event);
-      }
-      console.log("Event deleted", res);
-      req.flash("success", "Event wurde erfolgreich gelöscht");
-      return res.redirect(303, "/events");
-    })
-    .catch(err => {
-      console.log("Event not deleted", err);
-      if (err.kind === "ObjectId" || err.name === "NotFound") {
-        return res.status(404).send({
-          message: "event not found with id " + req.params.id
-        });
-      }
-      return res.status(500).send({
-        message: "Could not delete event with id " + req.params.id
-      });
-    });
-};
-
-function eventNotFoundByIdResponse(req, res, event) {
-  console.log("Event not found with id " + req.params.id);
-  req.flash("error", "Event konnte nicht gefunden werden");
-  res.render("event", { event: event, moment: moment });
+exports.createEvent = async function (req) {
+    try {
+        var event = assignParamsToEvent(req);
+        event._id = new mongoose.Types.ObjectId();
+        return await event.save()
+    } catch (e) {
+        // Log Errors
+        console.log(e);
+        throw Error('Error while Creating Event')
+    }
 }
+
+exports.findOne = async function (req) {
+    try {
+        return await Event.find(query)(req.params.id)
+    } catch (e) {
+        // Log Errors
+        console.log(e);
+        throw Error('Error while Getting Event')
+    }
+};
+
+exports.update = async function(req) {
+    try {
+        return await Event.findByIdAndUpdate(req.params.id, assignParamsToEvent(req), {new: true})
+    } catch (e) {
+        // Log Errors
+        console.log(e);
+        throw Error('Error while Updating Event')
+    }
+};
+
+exports.delete = async function(req) {
+    try {
+        return await Event.findByIdAndRemove(req.params.id)
+    } catch (e) {
+        // Log Errors
+        console.log(e);
+        throw Error('Error while Deleting Event')
+    }
+};
 
 function assignParamsToEvent(req) {
   return new Event({
