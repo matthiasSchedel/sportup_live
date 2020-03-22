b3dfe6267b915d1ad17c20ec1a89a5171a61f78b
talu-zz <talulu@gmx.de>
2020-03-22 11:55:24 +0100

    Adjust main to same layout as index

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
+var mainRouter = require('./routes/main');
 var usersRouter = require('./routes/users');
 var loginPageRouter = require('./routes/login');
 var signupPageRouter = require('./routes/signup');
@@ -141,17 +142,18 @@ app.get('/index.html', function (req, res, next) {
 app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login-page' }),
   function (req, res) {
-    res.redirect('/');
+    res.redirect('/main');
   });
 
 
 app.post('/signup',
   passport.authenticate('signup-local', { failureRedirect: '/signup-page' }),
   function (req, res) {
-    res.redirect('/');
+    res.redirect('/main');
   });
 
 app.use('/', indexRouter);
+app.use('/main', mainRouter);
 app.use('/events', eventsRouter);
 app.use('/users', usersRouter);
 
