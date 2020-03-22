f719960ffc46cb7636c6795a5d3da05ee218a647
Simon Bigelmayr <bigel86@gmail.com>
2020-03-22 02:22:04 +0100

    add event to landing page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var moment = require("moment");
+var EventService = require("../services/eventService");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+    try {
+        var events = await EventService.getEvents({})
+        res.render('index', { events: events, moment: moment});
+    } catch (e) {
+        req.flash("error", "Fehler beim event laden");
+        res.render("index", { events: [], moment: moment });
+    }
+
+  
 });
 
 module.exports = router;
